7e6bb4adaea7c444637d2fa0c1dd81682e44705e
boryslee <developer.clear@gmail.com>
2019-12-25 22:45:35 +0800

    add navbar in header

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,9 @@
 import React from "react";
-// import { Link } from "gatsby";
+import { Link as GatsbyLink, navigate } from 'gatsby';
 import PropTypes from "prop-types";
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
-import CameraIcon from '@material-ui/icons/PhotoCamera';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Fab from '@material-ui/core/Fab';
@@ -14,10 +13,52 @@ import HideOnScroll from './HideOnScroll';
 import ScrollTop from './ScrollTop';
 
 const useStyles = makeStyles(theme => ({
-  icon: {
-    marginRight: theme.spacing(2),
+  title: {
+    '&:hover': {
+      cursor: 'pointer',
+    }
   },
+  toolbar: {
+    margin: '0 auto',
+    maxWidth: 1200,
+    width: '100%'
+  },
+  navbar: {
+    listStyle: 'none',
+    textAlign: 'right',
+    marginLeft: 'auto',
+    '& li': {
+      display: 'inline',
+      padding: '0 0.5rem',
+    }
+  },
+  link: {
+    color: 'white',
+    textDecoration: 'none',
+  },
+  active: {
+    textDecoration: 'underline'
+  }
 }));
+
+const navLinks = [
+  {
+    to: '/',
+    title: 'About',
+  },
+  {
+    to: '/work',
+    title: 'Work',
+  },
+  {
+    to: '/blog',
+    title: 'Blog',
+  },
+  {
+    to: '/contact',
+    title: 'Contact',
+  },
+];
 
 const Header = (props) => {
   const classes = useStyles();
@@ -27,11 +68,17 @@ const Header = (props) => {
       <CssBaseline />
       <HideOnScroll {...props}>
         <AppBar>
-          <Toolbar>
-            <CameraIcon className={classes.icon} />
-            <Typography variant="h6" color="inherit" noWrap>
+          <Toolbar className={classes.toolbar}>
+            <Typography variant="h6" color="inherit" noWrap className={classes.title} onClick={() => navigate('/')}>
               {siteTitle}
             </Typography>
+            <ul className={classes.navbar}>
+              {navLinks.map((list) =>
+                <li>
+                  <GatsbyLink to={list.to} className={classes.link} activeClassName={classes.active}>{list.title}</GatsbyLink>
+                </li>
+              )}
+            </ul>
           </Toolbar>
         </AppBar>
       </HideOnScroll>
