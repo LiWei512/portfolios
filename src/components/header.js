1ad953bd3e9bbfaed015b21d8f7d76e52b441c0b
boryslee <developer.clear@gmail.com>
2019-12-25 18:29:32 +0800

    refactor header

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,20 @@
-import React from "react"
-// import { Link } from "gatsby"
-import PropTypes from "prop-types"
+import React from "react";
+// import { Link } from "gatsby";
+import PropTypes from "prop-types";
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
 import CameraIcon from '@material-ui/icons/PhotoCamera';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Slide from '@material-ui/core/Slide';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+
+import HideOnScroll from './HideOnScroll';
 
 const useStyles = makeStyles(theme => ({
   icon: {
     marginRight: theme.spacing(2),
   },
 }));
-
-function HideOnScroll({ children }) {
-  const trigger = useScrollTrigger();
-
-  return (
-    <Slide appear={false} direction="down" in={!trigger}>
-      {children}
-    </Slide>
-  );
-}
-
-HideOnScroll.propTypes = {
-  children: PropTypes.element.isRequired,
-};
 
 const Header = (props) => {
   const classes = useStyles();
