f3f8af6c7d3cec9f70e16b5af0911f51eb835167
boryslee <developer.clear@gmail.com>
2019-12-26 16:29:50 +0800

    remove unused component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,10 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Album from '../components/album'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Album />
   </Layout>
 )
 
