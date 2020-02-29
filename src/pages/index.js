4e14056b94bfa62fe6e7350c2eca9623e9469354
boryslee <developer.clear@gmail.com>
2020-02-29 20:15:17 +0800

    change home page template

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,16 @@
 import React from "react"
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
 import { graphql } from "gatsby";
 import Img from "gatsby-image";
 import Typography from '@material-ui/core/Typography';
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import SocialGroup from '../components/SocialGroup';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    marginTop: 12,
-    minHeight: 'calc(100vh - 64px)',
+    height: '100vh',
   },
   leftPaper: {
     minHeight: '100%',
@@ -29,6 +26,25 @@ const useStyles = makeStyles(theme => ({
     flexDirection: 'column',
     padding: '1rem',
   },
+  splashImageWrapper: {
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    width: '100%',
+    position: 'absolute !important',
+    overflow: 'hidden',
+  },
+  splashImage: {
+    height: '100%'
+  },
+  splashContent: {
+    position: 'relative',
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 }));
 
 const IndexPage = ({ data }) => {
@@ -37,28 +53,19 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       <SEO title="Home" />
-      <Container maxWidth="lg" className={classes.root}>
-        <Grid container spacing={3} style={{ minHeight: 'inherit' }}>
-          <Grid item xs={12} sm={6}>
-            <div className={classes.leftPaper}>
-              <Img
-                fixed={data.file.childImageSharp.fixed}
-                alt="Borys Lee"
-              />
-            </div>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <div className={classes.rightPaper}>
-              <Typography variant="h2" color="inherit">
-                About me
-              </Typography>
-              <Typography variant="h6" color="inherit">
-                {data.site.siteMetadata.description}
-              </Typography>
-              <SocialGroup />
-            </div>
-          </Grid>
-        </Grid>
+      <Container disableGutters maxWidth={false} className={classes.root}>
+        <div className={classes.splashImageWrapper}>
+          <Img
+            className={classes.splashImage}
+            fluid={data.file.childImageSharp.fluid}
+            alt="Borys Lee"
+          />
+        </div>
+        <Container maxWidth={"lg"} className={classes.splashContent}>
+          <Typography variant="h1" component="h1" align="center">
+            Hello World
+          </Typography>
+        </Container>
       </Container>
     </Layout>
   )
@@ -68,12 +75,12 @@ export default IndexPage
 
 export const query = graphql`
   query {
-    file(relativePath: { eq: "labtocat.png" }) {
+    file(relativePath: { eq: "landscape.jpg" }) {
       childImageSharp {
         # Specify a fixed image and fragment.
         # The default width is 400 pixels
-        fixed(width: 480, height: 600) {
-          ...GatsbyImageSharpFixed
+        fluid(maxWidth: 1920) {
+          ...GatsbyImageSharpFluid
         }
       }
     }
