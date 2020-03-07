b131f879dca19d6ffc43180ed6290e31e1a6b380
boryslee <developer.clear@gmail.com>
2020-03-08 00:17:36 +0800

    enhance footer section

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,14 +2,20 @@ import React from "react"
 import Typography from "@material-ui/core/Typography"
 import Box from "@material-ui/core/Box"
 import Emoji from "./Emoji"
+import gatsbyIcon from '../static/gatsby.svg';
 
 export default function Footer() {
   return (
     <footer>
       <Box p={6} bgcolor="secondary.main">
         <Typography variant="h6" align="center" gutterBottom>
-          Made with <Emoji symbol="❤" label="love" style={{ color: "red" }} /> and{" "}
-          <Emoji symbol="☕" label="coffee" />.
+          <Box component="span" display="flex" justifyContent="center" alignItems="center">
+            <span>Made with&nbsp;</span>
+            <img style={{ width: 20, height: 20 }} src={gatsbyIcon} alt="gatsby" /><span>,&nbsp; </span>
+            <Emoji symbol="❤" label="love" style={{ color: "red" }} /><span>, and&nbsp;</span>
+            <Emoji symbol="☕" label="coffee" />
+            {"."}
+          </Box>
         </Typography>
       </Box>
     </footer>
