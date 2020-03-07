d08b128be8107f981c0398235fd54709a1be8e38
boryslee <developer.clear@gmail.com>
2020-03-07 11:00:28 +0800

    fix some attributes in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,24 +1,17 @@
 import React from "react"
 import Typography from "@material-ui/core/Typography"
-import { makeStyles } from "@material-ui/core/styles"
+import Box from "@material-ui/core/Box"
 import Emoji from "./Emoji"
 
-const useStyles = makeStyles(theme => ({
-  footer: {
-    backgroundColor: theme.palette.secondary.main,
-    padding: theme.spacing(6),
-  },
-}))
-
 export default function Footer() {
-  const classes = useStyles()
-
   return (
-    <footer className={classes.footer}>
-      <Typography variant="h6" align="center" gutterBottom>
-        Made with <Emoji symbol="❤" lavel="love" style={{ color: "red" }} /> and{" "}
-        <Emoji symbol="☕" lavel="love" />.
-      </Typography>
+    <footer>
+      <Box p={6} bgcolor="secondary.main">
+        <Typography variant="h6" align="center" gutterBottom>
+          Made with <Emoji symbol="❤" label="love" style={{ color: "red" }} /> and{" "}
+          <Emoji symbol="☕" label="coffee" />.
+        </Typography>
+      </Box>
     </footer>
   )
 }
