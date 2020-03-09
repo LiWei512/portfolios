6514ff0d387be469ac7e3b6c6e0de87d7b3efa04
boryslee <developer.clear@gmail.com>
2020-03-09 18:32:35 +0800

    add timeline data

--- src/components/Sections/LifeTimeline.js
+++ src/components/Sections/LifeTimeline.js
@@ -2,7 +2,7 @@ import React from "react"
 import Container from "@material-ui/core/Container"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
-import { makeStyles } from "@material-ui/core/styles"
+import Box from "@material-ui/core/Box"
 import SchoolIcon from "@material-ui/icons/School"
 import WorkIcon from "@material-ui/icons/Work"
 import {
@@ -11,24 +11,7 @@ import {
 } from "react-vertical-timeline-component"
 import "react-vertical-timeline-component/style.min.css"
 
-const useStyles = makeStyles(theme => ({
-  item: {
-    display: "flex",
-    "& > img": {
-      width: 64,
-      height: 64,
-    },
-    "& > div": {
-      paddingLeft: "2rem",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-    },
-  },
-}))
-
 export default function LifeTimeline() {
-  const classes = useStyles()
   const data = useStaticQuery(graphql`
     query {
       allExperiencesJson {
@@ -75,11 +58,11 @@ export default function LifeTimeline() {
           <VerticalTimelineElement
             key={index}
             className="vertical-timeline-element--work"
-            date={item.node.date}
+            // date={item.node.date}
             iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
             icon={<WorkIcon />}
           >
-            <div className={classes.item}>
+            <Box display="flex">
               <Img
                 fixed={
                   item.node.logo
@@ -87,9 +70,14 @@ export default function LifeTimeline() {
                     : "https://via.placeholder.com/64"
                 }
                 alt={item.node.title}
-                className={classes.image}
+                style={{ width: 64, height: 64 }}
               />
-              <div>
+              <Box
+                paddingLeft="2rem"
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+              >
                 <h3 className="vertical-timeline-element-title">
                   {item.node.title}
                 </h3>
@@ -99,8 +87,8 @@ export default function LifeTimeline() {
                 <h4 className="vertical-timeline-element-subtitle">
                   {item.node.date}
                 </h4>
-              </div>
-            </div>
+              </Box>
+            </Box>
             <p>{item.node.description}</p>
           </VerticalTimelineElement>
         ))}
@@ -108,17 +96,21 @@ export default function LifeTimeline() {
           <VerticalTimelineElement
             key={index}
             className="vertical-timeline-element--education"
-            date={item.node.date}
+            // date={item.node.date}
             iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
             icon={<SchoolIcon />}
           >
-            <div className={classes.item}>
+            <Box display="flex">
               <Img
                 fixed={item.node.logo.childImageSharp.fixed}
                 alt={item.node.title}
-                className={classes.image}
               />
-              <div>
+              <Box
+                paddingLeft="2rem"
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+              >
                 <h3 className="vertical-timeline-element-title">
                   {item.node.title}
                 </h3>
@@ -128,8 +120,8 @@ export default function LifeTimeline() {
                 <h4 className="vertical-timeline-element-subtitle">
                   {item.node.date}
                 </h4>
-              </div>
-            </div>
+              </Box>
+            </Box>
             <p>{item.node.description}</p>
           </VerticalTimelineElement>
         ))}
