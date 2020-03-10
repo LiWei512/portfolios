3a6f7256740abcae454fe233b2abb34429a84317
boryslee <developer.clear@gmail.com>
2020-03-11 00:00:16 +0800

    add rubik font

--- src/gatsby-theme-material-ui-top-layout/theme.js
+++ src/gatsby-theme-material-ui-top-layout/theme.js
@@ -3,6 +3,18 @@ import { createMuiTheme } from "@material-ui/core/styles"
 
 // A custom theme for this app
 const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Rubik',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
   palette: {
     primary: {
       main: "#FEDBD0",
