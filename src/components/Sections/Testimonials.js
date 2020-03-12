792cc4e6fe276e6951fa2eda74ca0c304190aeac
boryslee <developer.clear@gmail.com>
2020-03-12 16:03:35 +0800

    reconfigure slider

--- src/components/Sections/Testimonials.js
+++ src/components/Sections/Testimonials.js
@@ -48,6 +48,9 @@ export default function Testimonials() {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false
   }
 
   return (
