afb485fdd1a17e3c29731f69ad8f7a6a562bacca
boryslee <developer.clear@gmail.com>
2020-03-12 17:46:17 +0800

    arrange assets

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,7 +77,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: "./data/",
+        path: `${__dirname}/data`,
       },
     },
     "gatsby-transformer-remark",
