efa4f874ed0104d201f40001613088e61529d44a
weili512 <developer.clear@gmail.com>
2020-05-01 02:14:08 +0800

    fix layout styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,31 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { useTheme } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core"
 
 import Header from "./header"
 import Footer from "./footer"
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  },
+  main: {
+    backgroundColor: theme.palette.primary.main,
+    flex: 'auto',
+  }
+}));
+
 const Layout = ({ children }) => {
-  const theme = useTheme()
+  const classes = useStyles();
+
   return (
-    <>
+    <div className={classes.root}>
       <Header />
-      <main style={{ background: theme.palette.primary.main }}>{children}</main>
+      <main className={classes.main}>{children}</main>
       <Footer />
-    </>
+    </div>
   )
 }
 
